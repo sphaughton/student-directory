a8a37b41c0c5d61704bcf4367df59697016662d3
Sean Haughton <seanphaughton@gmail.com>
2015-01-27 15:00:37 +0000

    Added interactive menu

--- student-directory/directory.rb
+++ student-directory/directory.rb
@@ -1,42 +1,64 @@
+
 def print_header
   puts "The students of my cohort at Makers Academy"
   puts "-------------"
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end
+  	students.each.with_index(1) do |student, index|
+    	puts "#{index}: #{student[:name]} (#{student[:cohort]} cohort)"
+  	end
 end
 
 def print_footer(names)    
-  puts "Overall, we have #{names.length} great students"
+  	puts "Overall, we have #{names.length} great students"
 end
 
 def input_students
-  	puts "Please enter the names of the students"
-  	puts "To finish, just hit return twice"
-
+	puts "Please enter the names of the students"
+	puts "To finish, just hit return twice"
   	# create an empty array
   	students = []
-
   	# get the first name
   	name = gets.chomp
-
   	# while the name is not empty, repeat this code
+  
   	while !name.empty? do    
     	# add the student hash to the array
     	students << {:name => name, :cohort => :november}    
     	puts "Now we have #{students.length} students"
+    
     	# get another name from the user
-    	name = gets.chomp
-	end
-
-	# return the array of students
-	students
+   	 	name = gets.chomp
+  end
+  
+  # return the array of students
+  students
 end
 
-students = input_students
-print_header
-print(students)
+def interactive_menu
+	loop do
+		# 1. print the menu and ask the user what to do
+		puts "1. Input the students"
+		puts "2. Show the students"
+		puts "9. Exit" # 9 because we'll be adding more items
+		# 2. read the input and save it into a variable
+		selection = gets.chomp
+		# 3. do what the user has asked
+		case selection
+		when "1"
+			students = input_students
+		when "2"
+			print_header
+			print(students)
+			print_footer(students)
+		when "9"
+			exit # this will cause the program to terminate
+		else
+			puts "I don't know what you meant, try again"
+		end
+	end
+end
+
+interactive_menu
 print_footer(students)
